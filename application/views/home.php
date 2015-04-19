0fc4a20d214f20d0664f842d6864d2f5538e6da2
Dmitry Ryzhkin <ryshkin@gmail.com>
2015-04-19 12:19:30 +0300

    home page

---
+++ application/views/home.php
@@ -0,0 +1,9 @@
+<div class="cm-logo"></div>
+<div class="cm-text">
+    Herzlich Willkommen auf der Webseite von der Band »CAROL MILE«. Hier finden Sie die Geschichte der Band, sowie die Musiker und Projekte,  beziehungsweise unsere Veranstaltungen. Ebenfalls können Sie unsere Videos und Fotos von Auftritten sehen und unsere musikalischen Kompositionen anhören. Außerdem bieten wir Ihnen unter „Service“ eine Reihe von verschiedenen Diensten, mit denen Sie sich gerne näher befassen können, wenn Sie weiteres Interesse haben. Wir wünschen Ihnen noch viel Spaß auf unserer Seite!
+    Mit freundlichen musikalischen Grüßen Ihre Band »CAROL MILE«.
+</div>
+<div class="cm-text">
+    Добро пожаловать на сайт музыкального ансамбля  »CAROL MILE«. Здесь Вы познакомитесь с историей группы, музыкантами, узнаете о наших музыкальных проектах, сможете посмотреть фото и видео, прослушать наши записи. Кроме этого, в разделе "сервис", мы предлагаем целый комплекс услуг, с которыми Вы так же сможете ознакомиться. Приятного путешествия по сайту!
+    С наилучшими музыкальными пожеланиями, Ваша группа »CAROL MILE«.
+</div>
