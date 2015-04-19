0517437ab8f5e7366be074a434851971a808f318
Dmitry Ryzhkin <ryshkin@gmail.com>
2015-04-19 13:29:26 +0300

    add band content

--- application/views/band.php
+++ application/views/band.php
@@ -1,1 +1,45 @@
-In progress ...
+<div class="cm-band-content">
+  <span class="cm-head">Carol Mile</span>
+  <div class="row">
+     <div class="col-6">
+        <p class="cm-band-content-text">
+            Die Band »Carol Mile« wurde im Jahr 2011 in München gegründet. Ihr Name kommt aus dem Englischen und bedeutet »fröhliches Meilenlied«. Dabei ist nur »Carol« als fröhliches Lied oder auch Hymne zu verstehen und »Mile« dagegen wird direkt als Meile wiedergegeben. Im Ukrainischen gibt es einen Analog des Wortes »Christmas Carol« (ukr.: Koljada), es ist eine Art von einem Volkslied, welches in der vorweihnachtlichen Zeit oder an Weihnachten gespielt wird. Eines der Projekte von dem Ensemble ist es Weihnachtslieder in einer moderneren Variante vorzuspielen. Außerdem tourt die Band innerhalb Deutschlands in verschiedenen Städten, tritt auf Hochzeiten, Geburtstagen, Weihnachten, Silvester und an Korporativen auf.
+        </p>
+        <p class="cm-band-content-text">
+            Am Ensemble beteiligen sich:
+        </p>
+        <p class="cm-band-content-text">
+            Antonina Kovalchuk – Gesang <br/>
+            Edward Myketin – künstlerischer Leiter, Multi-Instrumentalist, Schlagzeuger & Sänger, permanenter Teamleiter<br/>
+            Michael Koifman – Bass Gitarre, Rhythmus-Synthesizer, Sänger<br/>
+            Yuri Taranovich – Lead- und Rhythmus Gitarre<br/>
+            Vladimir Pavlov – Keyboard<br/>
+            Paul  Bishkoy – Gitarre, Akkordeon, Gesang, Arrangeur<br/>
+            Igor Vedeneev – Keyboarder<br/>
+            Jakub Zhurovsky – Violine, Trompete, Sänger<br/>
+            Mikhail Prizmazonov – Ton-Ingenieur
+        </p>
+     </div>
+     <div class="col-6">
+         <p class="cm-band-content-text">
+             Группа »Carol Mile« была основана в Мюнхене в 2011 году.
+             Интересна история возникновения названия группы »Carol Mile«: »Carol« в переводе с английского ознчает »веселая песнь« или же »гимн« (обычно - Рождество). »Mile« – это миля, так что в переводе название группы может звучать как »весёлая миля«. В украинском языке есть аналог слова »carol« – »коляда«, т.е. род особой народной песни, которую поют в предрождественский период и на Рождество. Один из проектов ансамбля – это исполнение Колядок в современных оранжировках. Кроме этого, группа гастолирует по городам Германии, сопровождает свадьбы, дни Рождения, Рождество и Новый год, а также выступает на корпоративах.
+         </p>
+         <p class="cm-band-content-text">
+             В группе учавствуют:
+         </p>
+         <p class="cm-band-content-text">
+             Антонина Ковальчук – вокал<br/>
+             Эдуард Микетин – художественный руководитель, барабанщик, мульти-инструменталист и певец<br/>
+             Михаил Койфман – бас-гитара, ритм-синтезатор, вокал<br/>
+             Юрий Таранович – соло и ритм-гитара<br/>
+             Владимир Павлов – клавишные<br/>
+             Павел Бишко – гитара, аккордеон, вокал, аранжировщик<br/>
+             Игорь Веденеев – клавишник<br/>
+             Якуб Журовский – скрипка, труба, вокал<br/>
+             Михаил Призмазонов – звукорежиссер
+         </p>
+
+     </div>
+  </div>
+</div>
