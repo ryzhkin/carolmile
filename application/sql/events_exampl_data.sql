a072c431e7e983aa1fe836b3b7c23dbffe0c8763
Dmitry Ryzhkin <ryshkin@gmail.com>
2015-04-28 13:25:34 +0300

    add demo data

---
+++ application/sql/events_exampl_data.sql
@@ -0,0 +1,36 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1
+-- Время создания: Апр 28 2015 г., 13:21
+-- Версия сервера: 5.5.25
+-- Версия PHP: 5.4.12
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `carolmile`
+--
+
+--
+-- Дамп данных таблицы `events`
+--
+
+INSERT INTO `events` (`id`, `event_date`, `title`, `poster`) VALUES
+(7, '2015-04-26', 'Пасе де лауэрте', 'b450b-afisha_84_cover-1-.jpg'),
+(8, '2015-04-27', 'Диск Краски', '17950-afisha-kraski4.jpg'),
+(9, '2015-04-28', 'Илья Лагутенко', '7538b-cover_200.jpg'),
+(10, '2015-04-29', 'Твин Пикс', '50754-d16649b817cb4a159fe75e3326ac.jpg'),
+(11, '2015-04-30', 'Кличко', '45361-klichko_-film-._afisha.jpg');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
