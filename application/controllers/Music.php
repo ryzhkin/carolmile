d3c9931c65349d67e1db327a21889e60dad04a48
Dmitry Ryzhkin <ryshkin@gmail.com>
2015-05-04 23:25:05 +0300

    adding music page

--- application/controllers/Music.php
+++ application/controllers/Music.php
@@ -4,14 +4,54 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Music extends MY_Controller {
   public function index()	{
      $this->load->database();
-     $query = $this->db->query('SELECT * FROM albom');
-     $alboms = $query->result();
-     $data = array(
-      'title'   => 'Music',
-      'content' =>  $this->load->view('music', array(
-         'alboms' => $alboms,
-       ), true)
-     );
-     $this->parser->parse('../../assets/index', $data);
-  }  
+     if ($this->input->is_ajax_request()) {
+       $result = array(
+         'ok' => true
+       );
+       switch ($this->input->post('ajaxAction')) {
+           case 'getAlbum': {
+
+             $sql = "
+             SELECT *
+             FROM `albom_track`
+             LEFT JOIN track on track.track_id = `albom_track`.`track_id`
+             WHERE
+              `albom_track`.`albom_id` = ".$this->input->post('albumID')."
+             ";
+             $query = $this->db->query($sql);
+             $tracks = $query->result();
+             $html = '<table>';
+             $counter = 1;
+             foreach ($tracks as $track) {
+               $mp3file = new MP3File("assets/uploads/mp3/".$track->mp3);
+               $duration = $mp3file->getDurationEstimate();//(faster) for CBR only
+               $songTime = floor($duration/60).':'.str_pad(($duration - floor($duration/60)*60), 2, '0', STR_PAD_LEFT);
+               //Common::xlog('mp3', $track->title.' > '.$songTime);
+               $html .= '
+                <tr class="'.(($counter == 1)?'active':'').'">
+                  <td>'.$counter.'. '.$track->title.'</td>
+                  <td style="max-width: 45px; width: 45px;">'.$songTime.'</td>
+                </tr>
+               ';
+               $counter++;
+             }
+             $html .= '</table>';
+             $result['html'] = $html;
+             break;
+           }
+       }
+       header('Content-Type: application/json');
+       echo json_encode($result);
+     } else {
+         $query = $this->db->query('SELECT * FROM albom');
+         $alboms = $query->result();
+         $data = array(
+             'title'   => 'Music',
+             'content' =>  $this->load->view('music', array(
+                     'alboms' => $alboms,
+                 ), true)
+         );
+         $this->parser->parse('../../assets/index', $data);
+     }
+  }
 }
--- application/views/music.php
+++ application/views/music.php
@@ -3,7 +3,39 @@
 
 
     <div class="albom-content">
+      <div class="row">
+        <div class="col-6">
+          <div class="cm-albom-cover">
 
+          </div>
+        </div>
+        <div class="col-6">
+            <div class="cm-albom-list">
+              <table>
+                <tr>
+                  <td>1. Name ....</td>
+                  <td>5:10</td>
+                </tr>
+                <tr class="active">
+                  <td>2. Name ....</td>
+                  <td>5:10</td>
+                </tr>
+                <tr>
+                  <td>3. Name ....</td>
+                  <td>5:10</td>
+                </tr>
+              </table>
+            </div>
+        </div>
+      </div>
+      <div class="cm-music-player">
+        <div class="cm-music-player-prev"></div>
+        <div class="cm-music-player-next"></div>
+        <div class="cm-music-player-progress">
+            <div class="cm-music-player-progress2"></div>
+        </div>
+        <div class="cm-music-player-time">10:00</div>
+      </div>
     </div>
     <div style="margin-top: 20px;">
         <div class="cm-gallery cm-alboms">
@@ -11,10 +43,11 @@
             foreach ($alboms as $albom) {
                 ?>
                 <div class="photo-contaner">
-                    <div class="photo" photo="<?= $albom->cover ?>" title="<?= $albom->title ?>" style="background-image: url('assets/uploads/cover/<?= $albom->cover ?>');"></div>
+                    <div albomID="<?= $albom->albom_id ?>" class="photo" photo="<?= $albom->cover ?>" title="<?= $albom->title ?>" style="background-image: url('assets/uploads/cover/<?= $albom->cover ?>');"></div>
                     <div class="photo-title">
                         <?= $albom->title ?>
                     </div>
+                    <data></data>
                 </div>
             <?php
             }
@@ -26,11 +59,41 @@
 <script type="text/javascript" src="<?= base_url() ?>assets/js/vendor/jplayer/jquery.jplayer.min.js"></script>
 <script type="text/javascript">
     jQuery(document).ready(function () {
+
+        var getAlbum = function (albumID) {
+          jQuery.post(window.location, {
+            ajaxAction : 'getAlbum',
+            albumID    : albumID
+          }, function (res) {
+             //console.log(res);
+             if (res.ok) {
+                if (typeof(res.html) == 'string') {
+                  jQuery('.cm-albom-list').html(res.html);
+                }
+             }
+          });
+        }
+
         jQuery('.cm-alboms').slick({
             //centerMode: true,
             infinite: true,
             slidesToShow: 5,
             slidesToScroll: 1
+        });
+        jQuery('.cm-gallery .photo').on('click', function () {
+            jQuery('.slick-slide').removeClass('current-photo');
+            jQuery(this).parent().addClass('current-photo');
+            jQuery('.cm-albom-cover').css('background-image', 'url("assets/uploads/cover/' + jQuery(this).attr('photo') + '")');
+
+            getAlbum(jQuery(this).attr('albomID'));
+        });
+
+        jQuery('.slick-active:first').addClass('current-photo');
+        jQuery('.cm-albom-cover').css('background-image', 'url("assets/uploads/cover/' + jQuery('.slick-active:first .photo').attr('photo') + '")');
+        getAlbum(jQuery('.slick-active:first .photo').attr('albomID'));
+
+        jQuery(window).on('resize', function () {
+          jQuery('.cm-music-player-progress').outerWidth(jQuery('.cm-music-player-progress').parent().outerWidth() - 140);
         });
 
     });
